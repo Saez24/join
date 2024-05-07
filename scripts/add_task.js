288f928dc3f1003a456c7b0dc6c329e242d7e8a4
Alex <saez0724+github@googlemail.com>
2024-05-07 22:08:49 +0200

    working on addtask

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,20 +1,3 @@
-function addSubtask() {
-    document.getElementById('addsubtask').style.display = 'none';
-    document.getElementById('subtask').style.display = 'block';
-}
-
-function closeAddSubtask() {
-    document.getElementById('addsubtask').style.display = 'block';
-    document.getElementById('subtask').style.display = 'none';
-}
-
-function handleSubtaskClick(event) {
-    var target = event.target;
-    if (target.tagName === "IMG" && target.src.includes("close.png")) {
-        closeAddSubtask();
-    }
-}
-
 function urgentButton() {
     let urgendButton = document.getElementById('urgent');
     urgendButton.style.background = '#FF3D00';
@@ -43,4 +26,54 @@ function lowButton() {
     urgendButton.innerHTML += /*html*/`
     Low <img src="./assets/img/prio_baja_white.png" alt="">
     `;
+}
+
+function openAddSubtaskField() {
+    document.getElementById('addsubtask').style.display = 'none';
+    document.getElementById('subtask').style.display = 'block';
+}
+
+function closeAddSubtaskField() {
+    document.getElementById('addsubtask').style.display = 'block';
+    document.getElementById('subtask').style.display = 'none';
+}
+
+function handleSubtaskClick(event) {
+    var input = document.getElementById("subtask");
+    var clickX = event.clientX;
+    var inputRight = input.getBoundingClientRect().right;
+
+    // Überprüfe, ob der Klick innerhalb des Bereichs des Bilds close.png liegt
+    if (clickX >= inputRight - 56 && clickX <= inputRight - 28) {
+        closeAddSubtaskField();
+    } else {
+        // Überprüfe, ob der Klick innerhalb des Bereichs des Bilds check_black.png liegt
+        if (clickX >= inputRight - 8) {
+            handleSubtaskClickAdd(event);
+        }
+    }
+}
+
+function handleSubtaskClickAdd(event) {
+    var input = document.getElementById("subtask");
+    var inputContent = input.value.trim();
+    if (inputContent !== "") {
+        var ul = document.getElementById("addsubtasks");
+        var li = document.createElement("li");
+        li.textContent = inputContent;
+        ul.appendChild(li);
+        closeAddSubtaskField();
+        input.value = "";
+    }
+}
+
+
+function addSubtask() {
+    var inputContent = document.getElementById("subtask").value;
+    if (inputContent.trim() !== "") {
+        var ul = document.getElementById("addsubtasks");
+        var li = document.createElement("li");
+        li.textContent = inputContent;
+        ul.appendChild(li);
+    }
 }
