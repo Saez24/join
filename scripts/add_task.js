bf40910a913b78575b08c66eef71df97dd01cb65
Alex <saez0724+github@googlemail.com>
2024-05-07 20:45:43 +0200

    working on addtask

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,11 +1,18 @@
 function addSubtask() {
     document.getElementById('addsubtask').style.display = 'none';
-    document.getElementById('aktive-subtask-block').style.display = 'block';
+    document.getElementById('subtask').style.display = 'block';
 }
 
 function closeAddSubtask() {
     document.getElementById('addsubtask').style.display = 'block';
-    document.getElementById('aktive-subtask-block').style.display = 'none';
+    document.getElementById('subtask').style.display = 'none';
+}
+
+function handleSubtaskClick(event) {
+    var target = event.target;
+    if (target.tagName === "IMG" && target.src.includes("close.png")) {
+        closeAddSubtask();
+    }
 }
 
 function urgentButton() {
@@ -13,7 +20,6 @@ function urgentButton() {
     urgendButton.style.background = '#FF3D00';
     urgendButton.style.color = '#FFFFFF';
     urgendButton.innerHTML = '';
-
     urgendButton.innerHTML += /*html*/`
     Urgend <img src="./assets/img/prio_alta_white.png" alt="">
     `;
@@ -24,7 +30,6 @@ function mediumButton() {
     urgendButton.style.background = '#FFA800';
     urgendButton.style.color = '#FFFFFF';
     urgendButton.innerHTML = '';
-
     urgendButton.innerHTML += /*html*/`
     Medium <img src="./assets/img/prio_media_white.png" alt="">
     `;
@@ -35,7 +40,6 @@ function lowButton() {
     urgendButton.style.background = '#7AE229';
     urgendButton.style.color = '#FFFFFF';
     urgendButton.innerHTML = '';
-
     urgendButton.innerHTML += /*html*/`
     Low <img src="./assets/img/prio_baja_white.png" alt="">
     `;
