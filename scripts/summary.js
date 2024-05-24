c17ccd0b5f6fcd68db680730a126abc49a052965
Julian W <111082172+Aynor123@users.noreply.github.com>
2024-05-24 14:27:22 +0200

    update summary JS

--- scripts/summary.js
+++ scripts/summary.js
@@ -7,25 +7,34 @@ let amountTasksInBoard = 0;
 let amountTasksInProgress = "";
 let amountTasksAwaitingFeedback = "";
 
-let upcomingDeadline = "";
+let earliestDeadline = null;
 
 let signedInUser = "";
 
 
-/** Fetches the data from firebase database from const "BASE_URL". This function can be given a certian path="" as parameter. For instance loadData('/tasks') explicity returns the sub-category "tasks" from the database.*/
+/** 
+ * Fetches the data from firebase database which represents the const "BASE_URL". 
+ * This function can be given a certian path="" as parameter. 
+ * For instance loadData('/tasks') explicity returns the sub-category "tasks" from the database.
+ * */
 async function loadData(path = "") {
     let response = await fetch(BASE_URL + path + ".json");
     responseToJson = await response.json();
     return responseToJson;
 }
 
-/** Updates the global variable "amountTasksInBoard", which will later be required to render the summary html page. */
+/** 
+ * Updates the global variable "amountTasksInBoard", which will later be required to render the summary html page. 
+ * */
 async function determineTasksInBoard() {
     let responseToJson = await loadData('/tasks');
     amountTasksInBoard = responseToJson.length;
 }
 
-/** Counts all urgent tasks and updates the global variable "amountTaskUrgent", which will later be required to render the summary html page. */
+/** 
+ * Counts all urgent tasks and updates the global variable "amountTaskUrgent", 
+ * which will later be required to render the summary html page. 
+ * */
 async function determineUrgentTasks() {
     let responseToJson = await loadData('/tasks');
     for (let i = 0; i < responseToJson.length; i++) {
@@ -36,7 +45,41 @@ async function determineUrgentTasks() {
 }
 
 
-/** Checks if the user has been visited the summary page during log-in. If so, the local storage key "greet" will be set to "no" and the good morning message will not be displayed. */
+/** 
+ * Determines the earliest deadline of all urgent tasks. 
+ * 
+ * Per default this function sets the global variable "earliestDeadline" to "null" in order to avoid an error when sorting 
+ * the date values from earliest to latest. 
+ * 
+ * If the global variale "earliestDeadline" remains "null" after fully iterating the for-loop to it's end, 
+ * the "earliestDeadline" will indicate, that no urgent tasks exist and thus no urgent deadline exists. Otherwise, the "earliestDeadline"
+ * will be formatted to an US-Date string.  
+ * */
+async function determineDeadline() {
+    earliestDeadline = null;
+    let responseToJson = await loadData('/tasks');
+
+    for (let i = 0; i < responseToJson.length; i++) {
+        if (responseToJson[i].prio === "urgent") {
+            let taskDueDate = new Date(responseToJson[i].duedate);
+
+            if (earliestDeadline === null || taskDueDate < earliestDeadline) {
+                earliestDeadline = taskDueDate;
+            }
+        }
+    }
+
+    if (earliestDeadline === null) {
+        earliestDeadline = "No urgent deadlines";
+    } else {
+        earliestDeadline = earliestDeadline.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+    }
+}
+
+
+/** Checks if the user has been visited the summary page during log-in. 
+ *  If so, the local storage key "greet" will be set to "no" and the good morning message will not be 
+ *  displayed again when the user re-visits the summary page. */
 function checkGreeting() {
     let greeter = document.getElementById("fade-out");
     if (localStorage.getItem('greet') === null) {
@@ -51,7 +94,6 @@ function checkGreeting() {
         setTimeout(function () {
             greeter.style.transition = "";
         }, 100);
-        // greeter.classList.add("d-none");
     }
 }
 
