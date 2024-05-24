7ebabd7c276a83f09c6819f35dc1422295674500
David Kolosza <david.kolosza@gmx.de>
2024-05-24 17:05:28 +0200

    contact information pure HTML

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -2,7 +2,7 @@ window.onload = getNames;
 let initialsBackgroundColors = [
     '#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8',
     '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701',
-    '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'
+    '#0038FF', '#00FFFF', '#FF00000', '#FF4646', '#FFBB2B'
 ];
 
 function slideInFromRight() {
