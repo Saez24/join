26c3472f5144bdf8da5a65d14414965c85a785df
Alex <saez0724+github@googlemail.com>
2024-05-28 15:02:56 +0200

    bugfixes + JSDOC

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDrm2QShTbbwiC0gpPDPP2LfdkdwQTZ5MI",
     authDomain: "remotestorage-b0ea0.firebaseapp.com",
@@ -13,23 +11,28 @@ const firebaseConfig = {
     appId: "1:997644324716:web:641faa74f9c4ddd39f2d49"
 };
 
-// Initialize Firebase
 let app = initializeApp(firebaseConfig);
 let auth = getAuth();
 let guestLogin = document.getElementById("guest-login");
 let login = document.getElementById("login");
 let errorContainer = document.getElementById('error');
-let errorInput = document.getElementById('password')
+let errorInput = document.getElementById('password');
 
+/**
+ * Handles the login button click event, signs in the user with email and password,
+ * and redirects to the summary page on success. Displays error message on failure.
+ * 
+ * @param {Event} event - The click event.
+ */
 login.addEventListener('click', function (event) {
-    event.preventDefault()
+    event.preventDefault();
 
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            window.location.href = "summary.html"
+            window.location.href = "summary.html";
         })
         .catch((error) => {
             let errorCode = error.code;
@@ -39,41 +42,58 @@ login.addEventListener('click', function (event) {
         });
 });
 
+/**
+ * Handles the guest login button click event, signs in the user anonymously,
+ * and redirects to the summary page on success. Displays error message on failure.
+ * 
+ * @param {Event} event - The click event.
+ */
 guestLogin.addEventListener('click', function (event) {
-    event.preventDefault()
+    event.preventDefault();
 
     signInAnonymously(auth)
         .then(() => {
-            window.location.href = "summary.html"
+            window.location.href = "summary.html";
         })
         .catch((error) => {
             let errorCode = error.code;
             let errorMessage = error.message;
-            // ...
         });
 });
 
+/**
+ * Sets the background image of the password input field to a lock icon.
+ */
 function loadPasswordImage() {
     let passwordImage = document.getElementById('password');
-    passwordImage.style.backgroundImage = 'url(../assets/img/lock.png)';
+    passwordImage.style.backgroundImage = 'url(./assets/img/lock.png)';
 };
 
 window.loadPasswordImage = loadPasswordImage;
 
+/**
+ * Sets the background image of the password input field to a visibility icon.
+ */
 function showPasswordImage() {
     let passwordImage = document.getElementById('password');
-    passwordImage.style.backgroundImage = 'url(../assets/img/visibility.png)';
+    passwordImage.style.backgroundImage = 'url(./assets/img/visibility.png)';
 };
 
 window.showPasswordImage = showPasswordImage;
 
+/**
+ * Hides the password image by setting a different background image.
+ */
 function hidePasswordImage() {
     let passwordImage = document.getElementById('password');
-    passwordImage.style.backgroundImage = 'url(../assets/img/visibility_off.png)';
+    passwordImage.style.backgroundImage = 'url(./assets/img/visibility_off.png)';
 };
 
 window.hidePasswordImage = hidePasswordImage;
 
+/**
+ * Toggles the password visibility.
+ */
 function showPassword() {
     let x = document.getElementById("password");
     if (x.type === "password") {
@@ -85,6 +105,9 @@ function showPassword() {
 
 window.showPassword = showPassword;
 
+/**
+ * Sets the password input type to "password".
+ */
 function hidePassword() {
     let x = document.getElementById("password");
     x.type = "password";
@@ -92,12 +115,19 @@ function hidePassword() {
 
 window.hidePassword = hidePassword;
 
+/**
+ * Handles the click event on the password input field to toggle visibility
+ * when clicking on the background image area.
+ * @param {MouseEvent} event - The mouse event.
+ */
 function handleShowpasswordClick(event) {
     let input = document.getElementById("password");
     let clickX = event.clientX;
     let inputRight = input.getBoundingClientRect().right;
-    if (clickX >= inputRight - 16) {
+
+    if (clickX >= inputRight - 30) {
         showPassword();
+        hidePasswordImage();
     }
 };
 
