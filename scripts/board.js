f35b29d0af798577115c7aedb057e886035a97ac
Alex <saez0724+github@googlemail.com>
2024-06-07 13:58:36 +0200

    Board drag and drop

--- scripts/board.js
+++ scripts/board.js
@@ -18,6 +18,7 @@ let taskIdCounter = 0;
 let currentDraggedElement = 0;
 let touchOffsetX = 0;
 let touchOffsetY = 0;
+let tasks = [];
 
 /**
  * Opens the dialog by removing the 'd_none' class and ensures CSS and content are loaded.
@@ -104,16 +105,16 @@ function loadAddTaskContent() {
 /**
  * Open the dialog for TaskDetails.
  */
-function showPopup(id) {
-    const popup = document.getElementById(id);
-    const taskDetails = popup.querySelector('.TaskDetails');
-
-    popup.classList.remove('hidden');
-    popup.classList.add('fade-in');
+function showPopup(task, taskid, assignedNamesHTML, subtaskCountHTML, priorityImage, categoryColor) {
+    const popup = document.getElementById('popup');
+    const taskDetails = document.getElementById('TaskDetailsDialog');
 
     setTimeout(() => {
+        popup.classList.remove('hidden');
+        popup.classList.add('fade-in');
         taskDetails.classList.add('slide-in-right');
     }, 300);
+    // renderTaskDialog(task, taskid, assignedNamesHTML, subtaskCountHTML, priorityImage, categoryColor);
 };
 
 /**
@@ -166,32 +167,55 @@ function generateRandomColor() {
 };
 
 /**
- * Generates HTML for assigned names.
+ * Generates HTML for assigned names with a 'Plus' button for overflow.
  * 
  * @function generateAssignedNamesHTML
  * @param {string[]} assignedNames - An array of names assigned to a task.
  * @returns {string} HTML string representing the assigned names.
  */
 function generateAssignedNamesHTML(assignedNames) {
-    return assignedNames.map(name => {
+    let MAX_NAMES_DISPLAYED = 3;
+    let position = 0;
+    let html = '';
+    let overflowCount = Math.max(0, assignedNames.length - MAX_NAMES_DISPLAYED);
+
+    assignedNames.slice(0, MAX_NAMES_DISPLAYED).forEach(name => {
         let initials = name.split(' ').map(n => n[0]).join('');
         let randomColor = generateRandomColor();
-        return /*html*/`
+        html += /*html*/`
             <div class="assignedName" style="background-color: ${randomColor};"><span>${initials}</span></div>`;
-    }).join('');
-};
+    });
+
+    if (overflowCount > 0) {
+        position += 110;
+        html += /*html*/ `
+            <div class="moreButtonBoard" style="left:${position}px">+${overflowCount}</div>`;
+    }
+
+    return html;
+}
+
 
 /**
- * Generates HTML for subtask count.
+ * Generates HTML for subtask count and progress bar.
  * 
  * @function generateSubtaskCountHTML
- * @param {Object[]} [subtask] - An array of subtasks.
- * @returns {string} HTML string representing the subtask count.
+ * @param {Object[]} [subtasks] - An array of subtasks.
+ * @returns {string} HTML string representing the subtask count and progress bar.
  */
-function generateSubtaskCountHTML(subtask) {
-    let count = subtask ? `${subtask.length}/${subtask.length} Subtask${subtask.length > 1 ? 's' : ''}` : '0/0 Subtasks';
-    return `<p class="subtaskCount">${count}</p>`;
-};
+function generateSubtaskCountHTML(subtasks) {
+    let totalSubtasks = subtasks ? subtasks.length : 0;
+    let completedSubtasks = subtasks ? subtasks.filter(subtask => subtask.completed).length : 0;
+    let progressPercentage = totalSubtasks > 0 ? (completedSubtasks / totalSubtasks) * 100 : 0;
+
+    let progressBarStyle = `width: ${progressPercentage}%;`;
+
+    let count = `${completedSubtasks}/${totalSubtasks} Subtask${totalSubtasks !== 1 ? 's' : ''}`;
+    let progressBarHTML = `<div class="progressBar"><div class="progress" style="${progressBarStyle}"></div></div>`;
+
+    return `<div class="subtaskProgress">${progressBarHTML}<p class="subtaskCount">${count}</p></div>`;
+}
+
 
 /**
  * Creates a task element.
@@ -223,11 +247,12 @@ function createTaskElement(task) {
  * @returns {string} HTML string representing the task element.
  */
 function createTaskHTML(task, taskid, assignedNamesHTML, subtaskCountHTML, priorityImage, categoryColor) {
+    let descriptionSection = task.description ? `<p class="descriptionBox">${task.description}</p>` : '';
     return /*html*/`
-        <div id="${taskid}" draggable="true" ondragstart="startDragging('${taskid}')" class="toDoBox" onclick="showPopup('popup')">
+        <div id="${taskid}" draggable="true" ondragstart="startDragging('${taskid}')" class="toDoBox" onclick="showPopup('${taskid}')">
             <button class="CategoryBox" style="background-color: ${categoryColor.background};">${task.category}</button>
             <p class="HeadlineBox">${task.title}</p>
-            <p class="descriptionBox">${task.description}</p>
+            <p class="descriptionBox">${descriptionSection}</p>
             <div class="subtaskProgress">
                 <progress value="0" max="100"></progress>
                 ${subtaskCountHTML}
@@ -292,6 +317,10 @@ function insertTasksIntoDOM(categorizedTasks) {
     document.querySelector('#inprogress .tasks').innerHTML = categorizedTasks.inprogress;
     document.querySelector('#awaitfeedback .tasks').innerHTML = categorizedTasks.awaitfeedback;
     document.querySelector('#done .tasks').innerHTML = categorizedTasks.done;
+    document.getElementById('todo').classList.remove('drag-area-highlight');
+    document.getElementById('inprogress').classList.remove('drag-area-highlight');
+    document.getElementById('awaitfeedback').classList.remove('drag-area-highlight');
+    document.getElementById('done').classList.remove('drag-area-highlight');
 }
 
 /**
@@ -382,13 +411,51 @@ async function updateTaskStatus(taskId, newStatus) {
     } catch (error) {
         console.error('Error updating task status:', error);
     }
-    removeHighlight(id)
 };
 
+/**
+ * Adds a highlight class to an element with the given ID.
+ * 
+ * This function selects an HTML element by its ID and adds the 'drag-area-highlight' 
+ * class to it, which can be used to apply specific styles defined in CSS.
+ * 
+ * @param {string} id - The ID of the element to highlight.
+ */
 function highlight(id) {
     document.getElementById(id).classList.add('drag-area-highlight');
-}
+};
 
+/**
+ * Removes a highlight class from an element with the given ID.
+ * 
+ * This function selects an HTML element by its ID and removes the 'drag-area-highlight' 
+ * class from it, which can be used to remove specific styles defined in CSS.
+ * 
+ * @param {string} id - The ID of the element to remove the highlight from.
+ */
 function removeHighlight(id) {
     document.getElementById(id).classList.remove('drag-area-highlight');
-}
+};
+
+// async function renderTaskDialog(task, taskid, assignedNamesHTML, subtaskCountHTML, priorityImage, categoryColor) {
+//     try {
+//         let tasks = await fetchData();
+//         let selectedTask = tasks.find(item => item.id === taskid);
+//         if (!selectedTask) {
+//             console.error(`Keine Aufgabe gefunden mit der id ${taskid}`);
+//             return;
+//         }
+
+
+//         let TaskDetailsDialog = document.getElementById('TaskDetailsDialog');
+
+//         TaskDetailsDialog.innerHTML = "";
+//         TaskDetailsDialog.innerHTML += /*html*/`
+//             <img id="${taskid}" class="closePopup" src="./assets/img/close.png" onclick="hidePopup('${taskid}')" alt="Close">
+//             <button id="CategoryBox" class="CategoryBox"></button>
+//         `;
+
+//     } catch (error) { }
+
+// };
+
