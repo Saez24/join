3c815bc71904fa880fdd098b2948c738064599d3
Alex <saez0724+github@googlemail.com>
2024-06-11 21:37:43 +0200

    working on task drop

--- scripts/board.js
+++ scripts/board.js
@@ -279,14 +279,8 @@ function createTaskHTML(task, taskid, assignedNamesHTML, subtaskCountHTML, prior
     } else {
         let descriptionSection = task.description ? `<p class="descriptionBox">${task.description}</p>` : '';
         return /*html*/`
-<div id="${taskid}" draggable="true" ondragstart="startDragging('${taskid}')" class="toDoBox" onclick="showPopup('${taskid}')">
-        <div class="headerContainer">
+        <div id="${taskid}" draggable="true" ondragstart="startDragging('${taskid}')" class="toDoBox" onclick="showPopup('${taskid}')">
             <button class="CategoryBox" style="background-color: ${categoryColor.background};">${task.category}</button>
-        <div class="arrowContainer">
-            <img src="./assets/img/arrow_drop.svg" class="arrow" alt="Arrow Drop">
-            <img src="./assets/img/arrow_drop_down.svg" class="arrow" alt="Arrow Drop Down">
-        </div>
-        </div>
             <p class="HeadlineBox">${task.title}</p>
             ${descriptionSection}
             <div class="subtaskProgress">
