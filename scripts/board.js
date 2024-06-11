5242dbbc3cd05e3b25c166c7639fddef87296285
Sarah Schneider <160104575+SarahSchneider1@users.noreply.github.com>
2024-06-11 15:45:18 +0200

    Board Responsive + Arrows Drag+Drop

--- scripts/board.js
+++ scripts/board.js
@@ -279,8 +279,14 @@ function createTaskHTML(task, taskid, assignedNamesHTML, subtaskCountHTML, prior
     } else {
         let descriptionSection = task.description ? `<p class="descriptionBox">${task.description}</p>` : '';
         return /*html*/`
-        <div id="${taskid}" draggable="true" ondragstart="startDragging('${taskid}')" class="toDoBox" onclick="showPopup('${taskid}')">
+<div id="${taskid}" draggable="true" ondragstart="startDragging('${taskid}')" class="toDoBox" onclick="showPopup('${taskid}')">
+        <div class="headerContainer">
             <button class="CategoryBox" style="background-color: ${categoryColor.background};">${task.category}</button>
+        <div class="arrowContainer">
+            <img src="./assets/img/arrow_drop.png" class="arrow" alt="Arrow Drop">
+            <img src="./assets/img/arrow_drop_down.png" class="arrow" alt="Arrow Drop Down">
+        </div>
+        </div>
             <p class="HeadlineBox">${task.title}</p>
             ${descriptionSection}
             <div class="subtaskProgress">
